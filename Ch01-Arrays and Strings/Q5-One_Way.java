057e84b38993f09f3e22321f18ad400c4a58de3b
unknown <jmls9627@gmail.com>
2020-07-27 13:32:03 -0400

    Ch1Q5-One_Way 2

--- Ch01-Arrays and Strings/Q5-One_Way.java
+++ Ch01-Arrays and Strings/Q5-One_Way.java
@@ -8,7 +8,7 @@ public static void main(String[] args) {
     }
 }
 
- public static boolean OneWay(String s1, String s2){         
+ public static boolean OneWay(String s1, String s2){         //always has to be s2 the one edit
    char elem=s2.charAt(0);
    int cant=0;  
    int pos=0;
@@ -44,4 +44,49 @@ if(s1.length()==s2.length()){    //if these are equals cant+1
 
 return true;
  }    
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+public static boolean oneEditAway(String first, String second) {
+    if (first.length() == second.length()) {
+        return oneEditReplace(first, second);
+    } else if (first.length() + 1 == second.length()) {
+        return oneEditInsert(first, second);
+    } else if (first.length() - 1 == second.length()) {
+        return oneEditInsert(second, first);
+    } 
+    return false;
+}
+
+public static boolean oneEditReplace(String s1, String s2) {
+    boolean foundDifference = false;
+    for (int i = 0; i < s1.length(); i++) {
+        if (s1.charAt(i) != s2.charAt(i)) {
+            if (foundDifference) {
+                return false;
+            }
+            
+            foundDifference = true;
+        }
+    }
+    return true;
+}
+
+/* Check if you can insert a character into s1 to make s2. */
+public static boolean oneEditInsert(String s1, String s2) {
+    int index1 = 0;
+    int index2 = 0;
+    while (index2 < s2.length() && index1 < s1.length()) {
+        if (s1.charAt(index1) != s2.charAt(index2)) {
+            if (index1 != index2) {
+                return false;
+            }		
+            index2++;
+        } else {
+            index1++;
+            index2++;
+        }
+    }
+    return true;
+}	
+
 }
