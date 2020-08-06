d0ccf45aaf04a2570672848f1a2300efc9b2d462
unknown <jmls9627@gmail.com>
2020-08-06 13:27:32 -0400

    Ch2Q5-Sum2

--- Ch02-Linked Lists/Q5-Sum.java
+++ Ch02-Linked Lists/Q5-Sum.java
@@ -6,22 +6,19 @@ public class CH2Linked {
         LinkedListNode first = new LinkedListNode(arr[0],null,null);
         LinkedListNode head = first;
         LinkedListNode second =first;
-      
-
+        
         for(int i=1;i<arr.length;i++){
             second=new LinkedListNode(arr[i],null,null);
             first.setNext(second);
             second.setPrevious(first);
             first=second;
         } 
-        
-        
+
           int [] arr1={2,9,5};
         LinkedListNode first1 = new LinkedListNode(arr1[0],null,null);
         LinkedListNode head1 = first1;
         LinkedListNode second1 =first1;
       
-
         for(int i=1;i<arr1.length;i++){
             second1=new LinkedListNode(arr1[i],null,null);
             first1.setNext(second1);
@@ -29,10 +26,11 @@ public class CH2Linked {
             first1=second1;
         } 
    
-      Sum(head, head1);
+        SumForward(head, head1);
+        SumBackward(head, head1);
 }
       
-     public static LinkedListNode Sum (LinkedListNode head, LinkedListNode head1){
+     public static LinkedListNode SumForward (LinkedListNode head, LinkedListNode head1){
        String val="";
        String val1="";
        LinkedListNode p1 =head;
@@ -40,11 +38,13 @@ public class CH2Linked {
   
         while(p1!=null){
             val+=p1.data;
-            p1=p1.next;}
+            p1=p1.next;
+        }
  
         while(p2!=null){
             val1+=p2.data;
-            p2=p2.next;}
+            p2=p2.next;
+        }
         
         int sum=Integer.parseInt(val)+ Integer.parseInt(val1);
         System.out.println(val+" + "+ val1 +" = "+sum); 
@@ -64,4 +64,39 @@ public class CH2Linked {
 
       return head2 ;
   }  
+  ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+  public static LinkedListNode SumBackward (LinkedListNode head, LinkedListNode head1){
+    String val="";
+    String val1="";
+    LinkedListNode p1 =head.last.next;
+    LinkedListNode p2 =head1.last.next;
+ 
+     while(p1!=null){
+         val+=p1.data;
+         p1=p1.prev;
+     }
+
+     while(p2!=null){
+         val1+=p2.data;
+         p2=p2.prev;
+     }   
+     int sum=Integer.parseInt(val)+ Integer.parseInt(val1);
+     System.out.println(val+" + "+ val1 +" = "+sum); 
+     String suma=Integer.toString(sum);   
+     int x=Integer.parseInt(String.valueOf(suma.charAt(suma.length()-1)));
+
+    LinkedListNode first=new LinkedListNode(x);
+    LinkedListNode head2 = first;
+    LinkedListNode second =first;
+      
+     for(int i=suma.length()-2;i>=0;i--){
+         second=new LinkedListNode(Integer.parseInt(String.valueOf(suma.charAt(i))));
+         first.setNext(second);
+         first=second;
+      }
+   System.out.println( head2.printForward());  
+
+   return head2 ;
+}  
 }
