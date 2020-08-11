84cd753b863d268cdeebc6dab553d8475eee9a65
unknown <jmls9627@gmail.com>
2020-08-11 13:22:26 -0400

    Ch3Q2-Min_value2

--- Ch03-Stacks and Queues/Q2-Min_Value.java
+++ Ch03-Stacks and Queues/Q2-Min_Value.java
@@ -20,3 +20,51 @@ public static int MinValue(Stack <Integer> pila){
    } 
  return val;
   }
+  ////////////////////////////////////////////////////////////////////////////
+
+  public static void main(String[] args) {
+    // TODO code application logic here
+    int [] arr={2,3,4,5,6,1,7,8,9,10};      
+    StackWithMin pilaMin=new StackWithMin();
+    for(int i=0;i<arr.length;i++){
+        pilaMin.push(arr[i]);
+    }
+         
+    while(!pilaMin.empty()){
+        System.out.println("Top " + pilaMin.peek()); 
+        System.out.println("Min " +pilaMin.min()); 
+         System.out.println();
+        pilaMin.pop();
+    }
+    
+}
+  public class StackWithMin extends Stack<Integer> {
+	Stack<Integer> p2;
+	
+	public StackWithMin() {
+		p2 = new Stack<Integer>();		
+	}
+	
+	public void push(int value){
+		if (value <= min()) {
+			p2.push(value);
+		}
+		super.push(value);
+	}
+	
+	public Integer pop() {
+		int value = super.pop();
+		if (value == min()) {
+			p2.pop();			
+		}
+		return value;
+	}
+	
+	public int min() {
+		if (p2.isEmpty()) {
+			return Integer.MAX_VALUE;
+		} else {
+			return p2.peek();
+		}
+	}
+}
