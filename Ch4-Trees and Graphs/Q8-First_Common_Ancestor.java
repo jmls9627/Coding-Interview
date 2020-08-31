f7ec1acde65fb8192d9cf22e8d2d5e73d3d3528c
unknown <jmls9627@gmail.com>
2020-08-31 14:48:44 -0400

    Ch4Q8-First_Common_Ancestor1

--- Ch4-Trees and Graphs/Q8-First_Common_Ancestor.java
+++ Ch4-Trees and Graphs/Q8-First_Common_Ancestor.java
@@ -40,5 +40,27 @@ public static int Depth(TreeNode var){
    }
   return depth;
 } 
+/////////////////////////////////////////////////////////////////////////////
+
+public static TreeNode commonAncestor(TreeNode p, TreeNode q) {
+    if (p == q) return p;
+    
+    TreeNode ancestor = p;
+    while (ancestor != null) {
+        if (isOnPath(ancestor, q)) {
+            return ancestor;
+        }
+        ancestor = ancestor.parent;
+    }
+    return null;
+}
+
+public static boolean isOnPath(TreeNode ancestor, TreeNode node) {
+    while (node != ancestor && node != null) {
+        node = node.parent;
+    }
+    return node == ancestor;
+}	
+///////////////////////////////////////////////////////////////
 
 }
