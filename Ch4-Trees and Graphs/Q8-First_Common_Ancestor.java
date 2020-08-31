978e6c824a50e2d169b55ba7a59ce439f93ab0be
unknown <jmls9627@gmail.com>
2020-08-31 15:11:57 -0400

    Ch4Q8-First_Common_Ancestor2

--- Ch4-Trees and Graphs/Q8-First_Common_Ancestor.java
+++ Ch4-Trees and Graphs/Q8-First_Common_Ancestor.java
@@ -62,5 +62,38 @@ public static boolean isOnPath(TreeNode ancestor, TreeNode node) {
     return node == ancestor;
 }	
 ///////////////////////////////////////////////////////////////
+public static TreeNode commonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    if (!covers(root, p) || !covers(root, q)) {
+        return null;
+    } else if (covers(p, q)) {
+        return p;
+    } else if (covers(q, p)) {
+        return q;
+    }
+    
+    TreeNode sibling = getSibling(p);
+    TreeNode parent = p.parent;
+    while (!covers(sibling, q)) {
+        sibling = getSibling(parent);
+        parent = parent.parent;
+    }
+    return parent;
+}
+
+public static boolean covers(TreeNode root, TreeNode p) { 
+    if (root == null) return false;
+    if (root == p) return true;
+    return covers(root.left, p) || covers(root.right, p); 
+}
+
+public static TreeNode getSibling(TreeNode node) {
+    if (node == null || node.parent == null) {
+        return null;
+    }
+    
+    TreeNode parent = node.parent;
+    return parent.left == node ? parent.right : parent.left;
+}
+    
 
 }
