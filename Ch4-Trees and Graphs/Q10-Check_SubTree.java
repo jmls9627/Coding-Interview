3c9eec7444634e4089c6f3efb217b79cd6672f61
unknown <jmls9627@gmail.com>
2020-09-02 16:28:20 -0400

    Ch4Q10-Check_Subtree1

--- Ch4-Trees and Graphs/Q10-Check_SubTree.java
+++ Ch4-Trees and Graphs/Q10-Check_SubTree.java
@@ -46,6 +46,40 @@ sb.append(node.data);           // Add root
 getOrderString(node.left, sb);  // Add left
 getOrderString(node.right, sb); // Add right
 }
+////////////////////////////////////////////////////////////////////////////
 
+public static boolean containsTree(TreeNode t1, TreeNode t2) {
+    if (t2 == null) {
+        return true; // The empty tree is a subtree of every tree.
+    }
+    return subTree(t1, t2);
+}
+
+/* Checks if the binary tree rooted at r1 contains the binary tree 
+ * rooted at r2 as a subtree somewhere within it.
+ */
+public static boolean subTree(TreeNode r1, TreeNode r2) {
+    if (r1 == null) {
+        return false; // big tree empty & subtree still not found.
+    } else if (r1.data == r2.data && matchTree(r1,r2)) {
+        return true;
+    }
+    return subTree(r1.left, r2) || subTree(r1.right, r2); 
+}
+
+/* Checks if the binary tree rooted at r1 contains the 
+ * binary tree rooted at r2 as a subtree starting at r1.
+ */
+public static boolean matchTree(TreeNode r1, TreeNode r2) {
+    if (r1 == null && r2 == null) {
+        return true; // nothing left in the subtree
+    } else if (r1 == null || r2 == null) { 
+        return false; // exactly one tree is empty, therefore trees don't match
+    } else if (r1.data != r2.data) {  
+        return false;  // data doesn't match
+    } else {
+        return matchTree(r1.left, r2.left) && matchTree(r1.right, r2.right);
+    }
+}
 
 }
