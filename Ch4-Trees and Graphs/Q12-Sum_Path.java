442d9f5d532f4ea9e473f936f4b575172ce4b1d8
unknown <jmls9627@gmail.com>
2020-09-04 20:14:09 -0400

    Ch4Q12-Sum_Path1

--- Ch4-Trees and Graphs/Q12-Sum_Path.java
+++ Ch4-Trees and Graphs/Q12-Sum_Path.java
@@ -41,3 +41,43 @@ public static int countPathsWithSumFromNode(TreeNode node, int targetSum, int cu
     
     return totalPaths;
 }	
+
+/////////////////////////////////////////////////////////////////////////
+
+public static int countPathsWithSum(TreeNode root, int targetSum) {
+    return countPathsWithSum(root, targetSum, 0, new HashMap<Integer, Integer>());
+}
+
+public static int countPathsWithSum(TreeNode node, int targetSum, int runningSum, HashMap<Integer, Integer> pathCount) {
+    if (node == null) return 0; // Base case
+    
+    runningSum += node.data;
+    
+    /* Count paths with sum ending at the current node. */
+    int sum = runningSum - targetSum;
+    int totalPaths = pathCount.getOrDefault(sum, 0);
+    
+    /* If runningSum equals targetSum, then one additional path starts at root. Add in this path.*/
+    if (runningSum == targetSum) {
+        totalPaths++;
+    }
+
+    /* Add runningSum to pathCounts. */
+    incrementHashTable(pathCount, runningSum, 1);
+    
+    /* Count paths with sum on the left and right. */
+    totalPaths += countPathsWithSum(node.left, targetSum, runningSum, pathCount);
+    totalPaths += countPathsWithSum(node.right, targetSum, runningSum, pathCount);
+    
+    incrementHashTable(pathCount, runningSum, -1); // Remove runningSum
+    return totalPaths;
+}
+
+public static void incrementHashTable(HashMap<Integer, Integer> hashTable, int key, int delta) {
+    int newCount = hashTable.getOrDefault(key, 0) + delta;
+    if (newCount == 0) { // Remove when zero to reduce space usage
+        hashTable.remove(key);
+    } else {
+        hashTable.put(key, newCount);
+    }
+}
