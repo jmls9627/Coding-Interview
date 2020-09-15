1c29c3a367fae89bf1473bb45b32b99020c61600
unknown <jmls9627@gmail.com>
2020-09-15 11:10:43 -0400

    Ch10Q1-BinarySearch1

--- Ch10-Sorting and Searching/Q1-Searching Algorithms.java
+++ Ch10-Sorting and Searching/Q1-Searching Algorithms.java
@@ -15,3 +15,22 @@ public static int BinarySearch(int[]arr, int data){
     }
     return -1;
 }
+//////////////////////////////////////////////////////
+
+public static int binarySearch(int[] a, int x) {
+    int low = 0;
+    int high = a.length - 1;
+    int mid;
+    
+    while (low <= high) {
+        mid = low + (high - low) / 2;
+        if (a[mid] < x) {
+            low = mid + 1;
+        } else if (a[mid] > x) {
+            high = mid - 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+}
