3ac14d6c2c3a7ba010f4ae7b0a7b2097b9c4d92e
unknown <jmls9627@gmail.com>
2020-09-16 17:39:30 -0400

    Ch10Q1-BinarySearch Recursive1

--- Ch10-Sorting and Searching/Q1-Searching Algorithms.java
+++ Ch10-Sorting and Searching/Q1-Searching Algorithms.java
@@ -48,3 +48,24 @@ public static int binarySearchRecursive(int[] a, int x, int low, int high) {
         return mid;
     }
 }
+/////////////////////////////////////////////////////////////////////////
+// Recursive algorithm to return the closest element
+public static int binarySearchRecursiveClosest(int[] a, int x, int low, int high) {
+    if (low > high) { // high is on the left side now
+        if (high < 0) return low;
+        if (low >= a.length) return high;
+        if (x - a[high] < a[low] - x) {
+            return high;
+        } 
+        return low;
+    }
+    
+    int mid = (low + high) / 2;
+    if (a[mid] < x) {
+        return binarySearchRecursiveClosest(a, x, mid + 1, high);
+    } else if (a[mid] > x) {
+        return binarySearchRecursiveClosest(a, x, low, mid - 1);
+    } else {
+        return mid;
+    }
+}	
