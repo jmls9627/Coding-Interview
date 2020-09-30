18178ab76427dbe336e24561739e4a334ddd15b6
unknown <jmls9627@gmail.com>
2020-09-30 12:15:55 -0400

    Ch00-Ex_11

---
+++ Ch00-Big_O/Ex_11.java
@@ -0,0 +1,17 @@
+public static void main(String[] args) {
+    int[] array = {0, 1, 2, 3, 4, 5};
+    reverse(array);
+    for (int x : array) {
+        System.out.println(x);
+    }
+}	
+
+public static void reverse(int[] array) {  //O(n)
+    for (int i = 0; i < array.length / 2; i++) {
+        int other = array.length - i - 1;
+        int temp = array[i];
+        array[i] = array[other];
+        array[other] = temp;
+    }
+}
+
