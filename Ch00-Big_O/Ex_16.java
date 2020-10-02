5637b81f2022275a84b24fb697677be68fefc354
unknown <jmls9627@gmail.com>
2020-10-02 13:33:03 -0400

    Ch00-Ex_16

---
+++ Ch00-Big_O/Ex_16.java
@@ -0,0 +1,20 @@
+
+public static void main(String[] args) {
+    permutation("abc");
+}	
+
+public static void permutation(String str) {
+    permutation(str, "");
+}
+
+public static void permutation(String str, String prefix) {//O(n)
+    if (str.length() == 0) {
+        System.out.println(prefix);
+    } else {
+        for (int i = 0; i < str.length(); i++) {
+            String rem = str.substring(0, i) + str.substring(i + 1);
+            permutation(rem, prefix + str.charAt(i));
+        }
+    }
+}
+
