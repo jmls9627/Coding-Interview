400716b44dc2a96cadae16c40f7e970730180dc8
unknown <jmls9627@gmail.com>
2020-12-01 16:02:33 -0500

    Ch00_Q6 Fix

--- Ch00-Big_O/Q6.java
+++ Ch00-Big_O/Q6.java
@@ -3,7 +3,7 @@ public static void main(String[] args) {
     System.out.println(sqrt(26));
 }
 
-public static int sqrt(int n) {//O(n)
+public static int sqrt(int n) {//O(sqrt(n))
     for (int guess = 1; guess * guess <= n; guess++) {
         if (guess * guess == n) {
             return guess;
